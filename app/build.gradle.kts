cd8e638c1a511e11de5daf3a9cc4bd7451d3428d
faric-ua <faric.eset@gmail.com>
2026-09-18 23:54:25 +0300

    chore: mark v1.4.38 R1 test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.saney.ytmimporter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 72
-        versionName = "1.4.38"
+        versionCode = 73
+        versionName = "1.4.38-R1"
     }
 
     signingConfigs {
