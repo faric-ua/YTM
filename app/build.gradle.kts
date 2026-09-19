e18d75ad380b8db868df1ce64eebd7c3b0ff2d4d
faric-ua <faric.eset@gmail.com>
2026-09-19 15:18:10 +0300

    release: bump corrective build to v1.4.41-R1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.saney.ytmimporter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 77
-        versionName = "1.4.41"
+        versionCode = 78
+        versionName = "1.4.41-R1"
     }
 
     signingConfigs {
