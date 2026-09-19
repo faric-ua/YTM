8fc80d270c621c7ec6f5a2e9ae493387fe81d78f
faric-ua <faric.eset@gmail.com>
2026-09-19 04:53:33 +0300

    release: bump YTM Importer to v1.4.41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.saney.ytmimporter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 76
-        versionName = "1.4.40"
+        versionCode = 77
+        versionName = "1.4.41"
     }
 
     signingConfigs {
