90adec5fc2ae412cc25cb832f0d1b3b956e52602
faric-ua <faric.eset@gmail.com>
2026-09-19 23:35:29 +0300

    chore: bump version to 1.4.46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.saney.ytmimporter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 85
-        versionName = "1.4.45"
+        versionCode = 86
+        versionName = "1.4.46"
     }
 
     signingConfigs {
