9d284f9e48e49e4950eead66abd6e28dce0f42a1
faric-ua <faric.eset@gmail.com>
2026-09-20 03:03:01 +0300

    chore: bump version to 1.4.47-R3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.saney.ytmimporter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 89
-        versionName = "1.4.47-R2"
+        versionCode = 90
+        versionName = "1.4.47-R3"
     }
 
     signingConfigs {
